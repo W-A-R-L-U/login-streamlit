39344230143ef29982acb5f08f71cde7242b0d03
Naalla Venkateshwarlu <114277372+W-A-R-L-U@users.noreply.github.com>
2024-07-16 08:02:50 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import webbrowser
+
+st.markdown("""
+<style>
+.css-nqowgj.e1ewe7hr3{
+visibility:hidden;
+}
+.css-h5rgaw.e1g8pov61{
+visibility:hidden;
+}
+</style>
+""",unsafe_allow_html=True)
+st.markdown("<h1 style='text-align:center'>Food selecty</h1>",unsafe_allow_html=True)
+with st.form("Login",clear_on_submit=True):
+    usr=st.text_input("Username")
+    pas=st.text_input("Password",type="password")
+    state=st.form_submit_button("LOGIN")
+    if state:
+        if usr=="":
+            st.warning("Fill the username")
+        elif pas=="":
+            st.warning("Don't act smart give the password")
+        else:
+            st.success("Redirecting...")
+            webbrowser.open("https://diet-recommendation-system.streamlit.app/")
+st.markdown("<p style='text-align:center'>New user <a href='http://localhost:8502/'>signup</a></p>",unsafe_allow_html=True)
